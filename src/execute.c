d4310def497f77e83c5c610fd3b5ad83110b8752
marineharutyunyan <m.marine.harutyunyan@gmail.com>
2023-02-26 16:30:37 +0400

    imitate pipes functionality with hardcoded 3 pipes

--- src/execute.c
+++ src/execute.c
@@ -1,41 +1,72 @@
 #include "mini.h"
 
-int change_ia_for_execve(int mode, int	in, int out)
-{
-	return (1);
-}
-
+// int change_ia_for_execve(int mode, int	in, int out) //mode tells if the pipes fd is the fist one or the last one or in the midle 
+// {
+// 	return (1);
+// }
+//  cat main.c | cat | cat
 int execute(t_general *g_data)
 {
 	pid_t id;
 	int     status;
 	int     i = 0;
-	int		fd[2];// fd[0] for read , fd[1] for write
+	int		*fd[2]; // TODO create pipecount fd with in and out using malloc
+	int		pipe1_fd[2];// fd[0] for read , fd[1] for write
+	int		pipe2_fd[2];// fd[0] for read , fd[1] for write
 	// printf("fsdg\n");
 	// g_data->pipes[0].argv[0] = "dsgdsg";
 	//  g_data->pipes[0].argv[1] = "-l";
-	pipe(fd);
-	printf("%d\n", fd[0]);
-	printf("%d\n", fd[1]);
-	// while (i < g_data->pipe_count)
-	// {  
-	// 	id = fork(); // TODO check ret_value;
-	// 	if(id == 0)
-	// 	{
-	// 		if (i == 0 && change_ia_for_execve(0), )
-	// 			execve("/bin/cat", g_data->pipes[i].argv, g_data->env);
-	// 		else if (i == 1 && change_ia_for_execve(1))
-	// 			execve("/bin/cat", g_data->pipes[i].argv, g_data->env);
-	// 		else if (change_ia_for_execve(2))
-	// 			execve("/bin/cat", g_data->pipes[i].argv, g_data->env);
-	// 		exit(5);
-	// 	} 
-	// 	i++;
-	// }
-	// waitpid(id, &status, 0);
+	pipe(pipe1_fd);
+	pipe(pipe2_fd);
+	// printf("pipe1_fd = %d\n", pipe1_fd[0]);
+	// printf("pipe1_fd = %d\n", pipe1_fd[1]);
+	// printf("pipe2_fd = %d\n", pipe2_fd[0]);
+	// printf("pipe2_fd = %d\n", pipe2_fd[1]);
+	while (i < g_data->pipe_count)
+	{  
+		id = fork(); // TODO check ret_value;
+		if(id == 0)
+		{
+			if (i == 0/* && change_ia_for_execve(0), */)
+			{
+				dup2(pipe1_fd[1], 1); 	
+				close(pipe1_fd[0]);
+	close(pipe1_fd[1]);
+	close(pipe2_fd[0]);
+	close(pipe2_fd[1]);
+				close(pipe1_fd[1]);
+				execve("/bin/cat", g_data->pipes[i].argv, g_data->env);
+			}
+			else if (i == 1/* && change_ia_for_execve(1)*/)
+			{
+				dup2(pipe1_fd[0], 0);
+				dup2(pipe2_fd[1], 1);
+				close(pipe1_fd[0]);
+	close(pipe1_fd[1]);
+	close(pipe2_fd[0]);
+	close(pipe2_fd[1]);
+				execve("/bin/cat", g_data->pipes[i].argv, g_data->env);
+			}
+			else /*if (change_ia_for_execve(2))*/
+			{
+				dup2(pipe2_fd[0], 0);
+	close(pipe1_fd[0]);
+	close(pipe1_fd[1]);
+	close(pipe2_fd[0]);
+	close(pipe2_fd[1]);
+				execve("/bin/cat", g_data->pipes[i].argv, g_data->env);
+			}
+			exit(5);
+		} 
+		i++;
+	}
+	close(pipe1_fd[0]);
+	close(pipe1_fd[1]);
+	close(pipe2_fd[0]);
+	close(pipe2_fd[1]);
+	waitpid(id, &status, 0);
 //     printf("status = %d\n", status);
 //     printf("id of parent %d\n", id);
-	
 //     // if (WIFEXITED(status))  // TODO exit_status
 //     // printf("%d\n", WEXITSTATUS(status)); //need to store the cheild exit status for
 	return(0);
--- src/main.c
+++ src/main.c
@@ -47,18 +47,20 @@ int	main(int argc, char **argv, char **env)
 	t_general	g_data;
 	char		*str;
 	g_data.env = env;
+	g_data.head_env = NULL;
+	set_env(&g_data, env);
 	while (1)
 	{
 		cmd = readline("Minishell$ ");
+		if (*cmd == '\0')
+			continue ;
 		add_history(cmd);
 		// if (!has_errors(cmd)) /* TODO enable erro check */
 		// ft_printf(1, "Line is valid\n"); //TODO add rediraction check
 		g_data.line = cmd;
-		g_data.head_env = NULL;
 		split_by_pipes(&g_data, &g_data.parse_data);
 		init_structs(&g_data);
 		paresing(&g_data);
-		set_env(&g_data, env);
 		//printf("str = %s\n", process_dollar_sign_and_quotes(g_data.line, &g_data));
 		handle_rediractions(&g_data);
 		execute(&g_data);
@@ -67,3 +69,24 @@ int	main(int argc, char **argv, char **env)
 	}
 	return (0);
 }
+
+
+// int main()
+// {
+// 	int	fd[2];
+// 	pipe(fd);
+// 	char c;
+// 	// write(fd[1], "2", 1);
+// 	write(fd[1], "2", 1);
+// 	read(fd[0], &c, 1);
+// 	printf("c = %c\n", c);
+// 	// int file1 = open("file1", O_RDWR);
+// 	// int file2 = open("file2", O_RDWR);
+// 	// printf("file1 = %d\n", file1);
+// 	// printf("file2 = %d\n", file2);
+// 	// printf("dup2 = %d\n", dup2(file1, file2));
+// 	// write(file2, "file1", 5);
+// 	// int	fd = dup(file1);
+// 	// printf("fd = %d\n", fd);
+// 	// printf("%d\n", write(fd, "file1_dup", 9));
+// }
